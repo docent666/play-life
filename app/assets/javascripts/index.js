dcad88358e84d43c2899e5acdace0c462ef992dd
Docent <lukasz.musial@gmail.com>
2014-01-03 00:01:31 +0000

    crude switch between auto and manual canvas refresh

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,22 +1,38 @@
+var doRefresh = false;
+
 $(function() {
-    $("#getStateButton").click(function(event) {
-        startRefresh();
+    $("#autoRefreshButton").click(function(event) {
+        if (doRefresh == false) {
+             doRefresh = true;
+             refresh();
+        }
+        else {
+            doRefresh = false;
+        }
+    })
+})
+
+$(function() {
+    $("#singleRefreshButton").click(function(event) {
+             doRefresh = false;
+             refresh();
     })
 })
 
 $(function() {
     $("#resetButton").click(function(event) {
         jsRoutes.controllers.LifeController.reset().ajax({
-                       success: function(data) {
-                            draw(data)
-                       }
+               success: function(data) {
+                 draw(data)
+               }
             })
     })
 })
 
 
-function startRefresh() {
-    setTimeout(startRefresh,50);
+function refresh() {
+    if (doRefresh == true)
+        setTimeout(refresh,50);
     jsRoutes.controllers.LifeController.getState().ajax({
                success: function(data) {
                  //  console.log(data)
--- test/MainControllerTest.java
+++ test/MainControllerTest.java
@@ -19,7 +19,7 @@ public class MainControllerTest {
                 assertThat(status(result)).isEqualTo(OK);
                 assertThat(contentType(result)).isEqualTo("text/html");
                 assertThat(charset(result)).isEqualTo("utf-8");
-                assertThat(contentAsString(result)).contains("Get next stage");
+                assertThat(contentAsString(result)).contains("Auto Refresh");
             }
         });
     }
