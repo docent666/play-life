a0cf7c523017809ccfa31c8d2e66cb189dd87cdb
Docent <lukasz.musial@gmail.com>
2014-02-01 18:54:03 +0000

    refactoring and organisation of 3d javascript

---
+++ app/assets/javascripts/3ddraw.js
@@ -0,0 +1,123 @@
+
+function padString(binString, targetWidth) {
+    var len = targetWidth-binString.length;
+    if (len>0)
+        return Array(targetWidth-binString.length+1).join("0")+binString;
+    return binString;
+}
+
+function createParticles(particleSystem, particles, planes, height, width, scene) {
+        for (var plane = 0; plane<planes.length; plane++) {
+                for (var x = 0; x < height; x++) {
+                      for (var y = 0; y < width*UNIT_WIDTH; y++) {
+                        var particle = new THREE.Vector3((x-height/2)*3, (y-width*UNIT_WIDTH/2)*3, (plane-10)*10);
+                        particles.vertices.push(particle);
+                     }
+                }
+            }
+        scene.add(particleSystem);
+}
+
+function draw(arrHex) {
+
+      var height = arrHex.length;
+      var width = arrHex[0].length;
+
+      if (particles.vertices.length==0) {
+         createParticles(particleSystem, particles, planes, height, width, scene)
+      }
+
+    var alphas = [];
+    for (var plane = 0; plane<planes.length; plane++) {
+          for (var i=0; i < height; i++) {
+                for (var j = 0; j<width;j++) {
+                    var paddedString = padString(arrHex[i][j].toString(2), UNIT_WIDTH);
+                    var rangeAlphas = Array.prototype.map.call(paddedString, function(x) { if (x==0) return 0.3; return 0; });
+                    alphas.push.apply(alphas, rangeAlphas)
+                }
+          }
+     }
+     attributes.alpha.value = alphas;
+     attributes.alpha.needsUpdate = true;
+
+}
+
+
+
+function animate(lastTime, particleSystem){
+        var time = (new Date()).getTime();
+        var timeDiff = time - lastTime;
+        var angleChange = angularSpeed * timeDiff * 2 * Math.PI / 1000;
+        for (i = 0; i<planes.length; i++) {
+             planes[i].rotation.z += angleChange;
+        }
+        particleSystem.rotation.z += angleChange;
+        lastTime = time;
+        renderer.render(scene, camera);
+        requestAnimationFrame(function() {
+            animate(lastTime, particleSystem);
+        });
+}
+
+
+var uniforms, attributes, particles;
+var angularSpeed = 0.05;
+
+var PLANE_WIDTH=424
+var PLANE_HEIGHT=300
+var UNIT_WIDTH=53;
+
+      // renderer
+      var renderer = new THREE.WebGLRenderer();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      document.body.appendChild(renderer.domElement);
+
+      //
+      var particles = new THREE.Geometry()
+
+
+         // attributes
+    attributes = {
+
+        alpha: { type: 'f', value: [] }
+
+    };
+
+    // uniforms
+    uniforms = {
+
+        color: { type: "c", value: new THREE.Color( 0xffffff ) }
+
+    };
+
+
+    var shaderMaterial = new THREE.ShaderMaterial( {
+
+        uniforms:       uniforms,
+        attributes:     attributes,
+        vertexShader:   document.getElementById( 'vertexshader' ).textContent,
+        fragmentShader: document.getElementById( 'fragmentshader' ).textContent,
+        transparent:    true
+
+    });
+
+      // camera
+      var camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 3000);
+      camera.position.y = -1050;
+      camera.position.z = 1200;
+      camera.rotation.x = 35 * (Math.PI / 180);
+
+var scene = new THREE.Scene();
+var planes = new Array();
+for (i = 0; i < 20; i++) {
+    planes[i] = new THREE.Mesh(new THREE.PlaneGeometry(PLANE_HEIGHT*3, PLANE_WIDTH*3), new THREE.MeshNormalMaterial({color: 0x77cae6, transparent: true, opacity: 0.1}));
+    planes[i].position.z = (i-10)*10;
+    planes[i].overdraw = true;
+    scene.add(planes[i]);
+}
+
+
+var particleSystem = new THREE.ParticleSystem(particles, shaderMaterial);
+
+
+animate(0, particleSystem, planes);
