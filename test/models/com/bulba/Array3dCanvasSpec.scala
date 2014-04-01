f51c10bc479b12820e5ece2fa199eda55bbb8198
Docent <lukasz.musial@gmail.com>
2014-04-01 17:57:27 +0100

    let's pretend that it was test-driven...

--- test/models/com/bulba/Array3dCanvasSpec.scala
+++ test/models/com/bulba/Array3dCanvasSpec.scala
@@ -54,6 +54,7 @@ class Array3dCanvasSpec extends FlatSpec with ShouldMatchers{
 
   "3d canvas" should "stage correctly" in {
     val staged = lay(1).stage()
+    staged.getCell(2,2) should be (DeadCell)
     staged.getCell(1,1) should be (LiveCell)
     staged.getCell(1,2) should be (LiveCell)
     staged.getCell(0,2) should be (DeadCell)
@@ -63,6 +64,18 @@ class Array3dCanvasSpec extends FlatSpec with ShouldMatchers{
      val deadCanvas = for (i <- 0 until 26) yield DeadCell
       lay(1).getNeighbors(-5, -5) should be (deadCanvas)
       lay(1).getNeighbors(10, 10) should be (deadCanvas)
+  }
+
+  "3d canvas" should "only stage cells for which neighbors changed in previous iteration" in {
+    lazy val lay: Layers[Seq[Cell], Seq[Seq[Cell]]]  = new Layers(for (i <- 0 until 3) yield new Array3dCanvas(canvases(i), i, lay, Set.empty))
+    val staged = lay(1).stage()
+    staged.canvas should be(lay(1).canvas)
+  }
+
+  "3d canvas" should "populate set of changed cells" in {
+    lay(1).stage().changedCells should be (Set((1,1)))
+    lay(0).stage().changedCells should be (Set((0,2), (0,0), (2,2), (0,1), (1,0)))
+    lay(2).stage().changedCells should be (Set((0,1), (0,2), (1,0), (2,1)))
   }
 
 }
